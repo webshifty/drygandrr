8c8acb31d49f6c0f9efd1e0fd1a113a463fcfa4c
Mishiko <webshifty@gmail.com>
2020-11-26 20:07:41 +0200

    0.0.36

---
+++ database/migrations/2020_11_26_175336_create_questions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('country')->default(0);
+            $table->integer('category')->default(0);
+            $table->string('question');
+            $table->integer('publish');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
