3c48e36752c5a9fde76dfcd06858001d11441e77
Mishiko <webshifty@gmail.com>
2020-11-26 19:22:41 +0200

    0.0.36

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -194,7 +194,7 @@ class BotController extends Controller
                         $menuCategories["inline_keyboard"] = $menuCat;
                     }
                     $reply = "Оберiть тематику питання";
-                    $client->debug($chatId, json_decode(dd(1)));
+                    $client->debug($chatId, $reply);
 
                     break;
                 /**
