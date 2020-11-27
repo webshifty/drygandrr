fb97574554eeb60937807bb1237582a043a5c1df
Mishiko <webshifty@gmail.com>
2020-11-27 18:03:28 +0200

    0.0.65

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -126,11 +126,10 @@ class BotController extends Controller
                 case !is_null($update->message->text):
                     if (strpos($text, '/consul') !== false) {
                         $userQuestion = str_replace('/consul', "", $text);
-                        //$base = TelegramBotData::saveUserQuestion($chatId, $client->easy->from_id, $userQuestion);
+                        TelegramBotData::saveUserQuestion($chatId, $client->easy->from_id, $userQuestion);
                         $client->sendPhoto($chatId, asset('/img/telegram/byebye1.png'));
-                        $reply2 = json_encode($chatId . $client->easy->from_id . $userQuestion);
                         $reply = "В нашій базі немає відповіді на це питання. Я все передала консулу. Він повернеться з відповіддю в свої робочі години.";
-                        $client->sendMessage($chatId, $reply2, null, null, null, null, null, null, $menuInBase);
+                        $client->sendMessage($chatId, $reply, null, null, null, null, null, null, $menuInBase);
                         exit();
                     } else {
                         $addCountry = TelegramBotData::addCountry($client->easy->from_id, $text);
