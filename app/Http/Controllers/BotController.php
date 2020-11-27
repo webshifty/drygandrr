1651aecc90eddbb29ac4eb9cb72e1fe21c3c803f
Mishiko <webshifty@gmail.com>
2020-11-27 05:15:11 +0200

    0.0.51

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -37,7 +37,7 @@ class BotController extends Controller
         $client = new Client(Config::get('telegram.bots.mybot.token'), false);
         $update = $client->getUpdate();
 
-        $reply2 = json_encode($update);
+        $reply2 = json_encode($update->message);
         $client->sendMessage($client->easy->chat_id, $reply2, 'HTML');
         exit();
 /**
