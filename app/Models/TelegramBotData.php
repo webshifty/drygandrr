6e990e091828802c4ccb2312d6fd63fe69d9817d
Mishiko <webshifty@gmail.com>
2020-12-11 21:14:04 +0200

    0.1.34

--- app/Models/TelegramBotData.php
+++ app/Models/TelegramBotData.php
@@ -92,7 +92,7 @@ class TelegramBotData extends Model
 
     public static function getQuestionByCountryByCategory($userId, $category)
     {
-        $userCountry = DB::table('tg_users')->select('tg_users.country', 'countries.id')->where('tg_id', $userId)->join('countries', 'name', 'LIKE', 'tg_users.country')->first();
+        $userCountry = DB::table('tg_users')->select('tg_users.country', 'countries.id')->where('tg_id', $userId)->join('countries', 'name', '=', 'tg_users.country')->first();
 
         if (is_null($userCountry)){
             $questions = DB::table('questions')->select('id','question')->where('category', $category)->get()->toArray();
